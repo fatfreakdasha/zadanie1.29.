10c1ff8d6a02e29a3330d8b7bb0d5c09608e23e9
Дарья <@LAPTOP-E9K2RNCV>
2022-09-26 20:10:39 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double R = Convert.ToDouble(Console.ReadLine());
+
+            double L = 2 * 3.14 * R;
+            double S = 3.14 * Math.Pow(R, 2);
+            Console.WriteLine("{0:0.00} {1:0.00}", L, S);
+            Console.ReadKey();
+
+        }
+    }
+}
+
